5d755e828f45a72be626632ca802e9e0143cab4a
Darina_Nikolaeva <Darina_Nikolaeva@epam.com>
2022-10-09 17:22:58 +0300

    added password verify

--- kata/phone_number.py
+++ kata/phone_number.py
@@ -7,3 +7,18 @@ def phone_number_verify(phones: List[str]) -> List[bool]:
     for phone in phones:
         result.append(re.match("[7-8][0-9]{9}$", phone) is not None)
     return result
+
+
+def password_verity(password: str) -> bool:
+    return hasCapitalLetter(password) and hasDigit(password)
+
+
+def hasCapitalLetter(password):
+    return hasPattern('[A-Z]*', password)
+
+
+def hasDigit(password):
+    return hasPattern('\\d', password)
+
+def hasPattern(pattern, password):
+    return re.search(pattern, password) is not None
--- test/photo_number_test.py
+++ test/photo_number_test.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from kata.phone_number import phone_number_verify
+from kata.phone_number import password_verity
 import codewars_test as test
 
 # ['89112360136', '9112360136', '911-234-12-45', '71q23455678']
@@ -23,3 +24,8 @@ def tests():
     def test_all_digits():
         given: List[str] = ["8888888888", "7a7777d777"]
         test.assert_equals(phone_number_verify(given), [True, False])
+
+    @test.it('')
+    def test_password_at_least_one_big_letter():
+        given = 'passw0rD'
+        test.assert_equals(password_verity(given), True)
