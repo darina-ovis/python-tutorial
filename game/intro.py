e3ffa43bedfaa79a09a197837f7d5e49e37ac57e
dnikolaeva <dnikolaeva@telematika.com>
2022-10-16 21:43:02 +0300

    added filled rect and animation during jump

--- game/intro.py
+++ game/intro.py
@@ -1,43 +1,45 @@
 import pgzrun
 import pygame
+
 from pgzero.actor import Actor
+from pgzero.animation import animate, decelerate
 from pgzero.clock import clock
 from pgzero.loaders import sounds
+from pgzero.rect import Rect
 
-WIDTH = 1280 # ось X
-HEIGHT = 720 # ось Y
+WIDTH = 1280  # ось X
+HEIGHT = 720  # ось Y
 CIRCLE_IMAGE = 'alien'
 circle = Actor(CIRCLE_IMAGE)
 # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
-circle.pos = (600, 500)
-toRight = True
+circle.pos = 600, 500
+is_moving_to_right = True
 
-RED = 200, 0, 0
-BOX = Rect((0, 620), (WIDTH, 100))
+DARK_RED_COLOUR = 139, 0, 0
+ground = Rect((0, HEIGHT - 100), (WIDTH, 100))
 
 
 def draw():
     screen.clear()
     # screen.fill((red, 0, 100))
     screen.blit('background', (0, 0))
-    screen.draw.rect(BOX, RED)
 
+    screen.draw.filled_rect(ground, DARK_RED_COLOUR)
     circle.draw()
 
 
 def update():
-    global toRight, BOX
-
+    global is_moving_to_right, ground
     step = 3
-    if toRight:
+    if is_moving_to_right:
         circle.x += step
     else:
         circle.x -= step
     alien_half_width = circle.width / 2
     if circle.x > WIDTH - alien_half_width or circle.x < alien_half_width:
-         toRight = not toRight
-    if not circle.colliderect(BOX):
-        circle.y += 1
+        is_moving_to_right = not is_moving_to_right
+    if not circle.colliderect(ground):
+        circle.y += step
 
 
 def on_mouse_down(pos):
@@ -46,24 +48,30 @@ def on_mouse_down(pos):
 
 
 def on_key_up(key):
-    print(key)
     if key == keys.RIGHT:
         circle.x += 10
     elif key == keys.DOWN:
         circle.x += 10
     if key == keys.UP:
-        circle.y -= 200
+        animate(circle, 'decelerate', 2, jump())
+
+
+def jump():
+    circle.y -= 50
 
 def set_alien_hurt():
-    circle._surf = pygame.transform.rotate(circle._surf, 180)
-    # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
+    circle.image = 'alien_hurt'
+    circle.angle += 180
     circle.y -= 20
     sounds.eep.play()
     clock.schedule_unique(set_alien_normal, 0.2)
 
+
 def set_alien_normal():
-    circle._surf = pygame.transform.rotate(circle._surf, 180)
+    circle.image = CIRCLE_IMAGE
+    circle.angle = 0
     # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
-    circle.y += 20
+    print(ground.top)
+
 
 pgzrun.go()
