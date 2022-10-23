e63f4e8d8208543a18ac65bf7ca28941fbf0c50a
dnikolaeva <dnikolaeva@telematika.com>
2022-10-23 19:17:47 +0300

    added player + stars

--- game/intro.py
+++ game/intro.py
@@ -1,77 +1,121 @@
+import random
+
 import pgzrun
 import pygame
 
 from pgzero.actor import Actor
 from pgzero.animation import animate, decelerate
 from pgzero.clock import clock
-from pgzero.loaders import sounds
+from pgzero.loaders import sounds, images
 from pgzero.rect import Rect
+
+JUMP_HIGHT = 80
 
 WIDTH = 1280  # ось X
 HEIGHT = 720  # ось Y
-CIRCLE_IMAGE = 'alien'
-circle = Actor(CIRCLE_IMAGE)
+CIRCLE_IMAGE = 'player_green'
+player = Actor(CIRCLE_IMAGE)
 # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
-circle.pos = 600, 500
+player.pos = 600, 500
 is_moving_to_right = True
 
+sky_color_red = 0
+sky_color_green = 0
+sky_color_blue = 100
+from_dark_to_light = True
 DARK_RED_COLOUR = 139, 0, 0
 ground = Rect((0, HEIGHT - 100), (WIDTH, 100))
+
+forest = []
+for i in range(18):
+    forest.append(
+        Actor('cactus', topleft=(images.cactus.get_width() * i + 10, HEIGHT - images.grass.get_height() - 100))
+    )
+
+stars = []
+for i in range(10):
+    stars.append(
+        Actor('star', topleft=(random.randint(0, 200) * i, random.randint(0, 400)))
+    )
 
 
 def draw():
     screen.clear()
-    # screen.fill((red, 0, 100))
-    screen.blit('background', (0, 0))
+    sky_color = (sky_color_red, sky_color_green, sky_color_blue)
+    screen.fill(sky_color)
+    screen.blit('background_tr', (0, 0))
+    screen.draw.rect(ground, DARK_RED_COLOUR)
 
-    screen.draw.filled_rect(ground, DARK_RED_COLOUR)
-    circle.draw()
+    for grass in forest:
+        grass.draw()
+
+    for star in stars:
+        star.draw()
+
+    player.draw()
 
 
 def update():
-    global is_moving_to_right, ground
+    global is_moving_to_right, ground, sky_color_blue, sky_color_red, sky_color_green, from_dark_to_light
+
+    if (from_dark_to_light):
+        sky_color_blue = (sky_color_blue + 1) % 255
+        sky_color_red = (sky_color_red + 1) % 255
+        sky_color_green = (sky_color_green + 1) % 255
+        if sky_color_green == 150:
+            from_dark_to_light = False
+    else:
+        sky_color_blue = (sky_color_blue - 1) % 255
+        sky_color_red = (sky_color_red - 1) % 255
+        sky_color_green = (sky_color_green - 1) % 255
+        if sky_color_green == 0:
+            from_dark_to_light = True
+
+    print(sky_color_green)
+
     step = 3
     if is_moving_to_right:
-        circle.x += step
+        player.x += step
     else:
-        circle.x -= step
-    alien_half_width = circle.width / 2
-    if circle.x > WIDTH - alien_half_width or circle.x < alien_half_width:
+        player.x -= step
+    alien_half_width = player.width / 2
+    if player.x > WIDTH - alien_half_width or player.x < alien_half_width:
         is_moving_to_right = not is_moving_to_right
-    if not circle.colliderect(ground):
-        circle.y += step
+    if not player.colliderect(ground):
+        player.y += step
 
 
 def on_mouse_down(pos):
-    if circle.collidepoint(pos):
+    if player.collidepoint(pos):
         set_alien_hurt()
 
 
 def on_key_up(key):
     if key == keys.RIGHT:
-        circle.x += 10
+        player.x += 10
     elif key == keys.DOWN:
-        circle.x += 10
+        player.x += 10
     if key == keys.UP:
-        animate(circle, 'decelerate', 2, jump())
+        animate(player, 'decelerate', 2, jump())
 
 
 def jump():
-    circle.y -= 50
+    if player.colliderect(ground):
+        player.y -= JUMP_HIGHT
+
 
 def set_alien_hurt():
-    circle.image = 'alien_hurt'
-    circle.angle += 180
-    circle.y -= 20
+    player.image = 'player_red'
+    # circle.angle += 180
+    # circle.y -= 20
     sounds.eep.play()
     clock.schedule_unique(set_alien_normal, 0.2)
 
 
 def set_alien_normal():
-    circle.image = CIRCLE_IMAGE
-    circle.angle = 0
+    player.image = CIRCLE_IMAGE
+    player.angle = 0
     # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
-    print(ground.top)
 
 
 pgzrun.go()
