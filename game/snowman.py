19b1763d107a1c08944adeda063b019a9546c3db
dnikolaeva <dnikolaeva@telematika.com>
2022-11-27 19:34:53 +0300

    created class snowman

--- game/snowman.py
+++ game/snowman.py
@@ -1,25 +1,15 @@
-from pgzero import loaders
 from pgzero.actor import Actor
+
 
 class Snowman(Actor):
     transparent_step = 2
 
-    is_moving_to_right = True
-    is_jumping = False
-
-    @property
-    def image(self):
-        return self._image_name
-
-    @image.setter
-    def image(self, image):
-        self._image_name = image
-        self._orig_surf = self._surf = loaders.images.load(image).convert_alpha()
-        self._update_pos()
+    def __init__(self, image, **kwargs):
+        super().__init__(image, **kwargs)
+        self.is_moving_to_right = True
+        self.is_jumping = False
 
     def jump(self, ground):
-        global is_jumping , is_moving_to_right
-        if self.colliderect(ground) :
+        if self.colliderect(ground):
             self.image = "snowman_up" if self.is_moving_to_right else 'snowman_up_left'
-            print(f"is_moving_to_right {self.is_moving_to_right} {self.image}")
             self.is_jumping = True
