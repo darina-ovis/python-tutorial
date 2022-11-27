afbd284d1c7b9e9841c2ee527370dd2fdc773c7b
dnikolaeva <dnikolaeva@telematika.com>
2022-11-27 21:09:49 +0300

    added score

--- game/intro.py
+++ game/intro.py
@@ -1,31 +1,27 @@
 import random
 import pgzrun
-import pygame
+from pgzero.loaders import sounds
+
 from snowman import Snowman
 
 from pgzero.actor import Actor
-from pgzero.animation import animate, decelerate
-from pgzero.clock import clock
-from pgzero.loaders import sounds, images
 from pgzero.rect import Rect
 
 from game.star import Star
 
-JUMP_HIGHT = 80
-JUMP_HIGHT = 200
-
 HEIGHT = 720  # ось Y
 WIDTH = 1280  # ось X
-PLAYER_IMAGE = 'snowman_right'
-player = Snowman(PLAYER_IMAGE)
-# circle._surf = pygame.transform.scale(circle._surf, (100, 100))
-player.pos = 600, 500
+DARK_RED_COLOUR = 139, 0, 0
+ground = Rect((0, HEIGHT - 100), (WIDTH, 100))
+player = Snowman('snowman_right', ground=ground)
+
+player.pos = 100, 600
 sky_color_red = 0
 sky_color_green = 0
 sky_color_blue = 100
 from_dark_to_light = True
-DARK_RED_COLOUR = 139, 0, 0
-ground = Rect((0, HEIGHT - 100), (WIDTH, 100))
+
+points = 300
 
 stars = []
 for i in range(30):
@@ -38,9 +34,9 @@ for i in range(40):
     )
 
 torches = []
-for i in range(3):
+for i in range(30):
     torches.append(
-        Actor('torch', bottomleft=(100+400 * i, HEIGHT - 100))
+        Actor('torch', bottomleft=(200 + 400 * i, HEIGHT - 100))
     )
 
 
@@ -62,10 +58,15 @@ def draw():
         torch.draw()
 
     player.draw()
+    screen.draw.text(f"Score: {points}", midleft=(50, 50), fontsize=50, shadow=(2, 2), scolor="#202020")
+
+    if points <= 0:
+        screen.draw.text("Game Over", center=(WIDTH/2, HEIGHT/2), fontsize=70, shadow=(2, 2), scolor="#202020")
+
 
 
 def update():
-    global  ground, sky_color_blue, sky_color_red, sky_color_green, from_dark_to_light
+    global ground, sky_color_blue, sky_color_red, sky_color_green, from_dark_to_light, points
 
     # sky
     if from_dark_to_light:
@@ -93,64 +94,39 @@ def update():
         star.change_transparency(from_dark_to_light)
 
     # player
-    step = 3
-    if player.is_moving_to_right:
-        player.x += step
-    else:
-        player.x -= step
-    player_half_width = player.width / 2
-    if player.x > WIDTH - player_half_width or player.x < player_half_width:
-        player.is_moving_to_right = not player.is_moving_to_right
-        flip_image()
-    if not player.colliderect(ground) or player.is_jumping:
-        if not player.is_jumping:
-            player.y += step
+    # player.update_x()
 
-        if player.y <= HEIGHT-100-JUMP_HIGHT:
+    height_y = 5 * player.y / (HEIGHT - 100 - player.JUMP_HEIGHT)
+    if player.is_jumping:
+        if player.y <= HEIGHT - 100 - player.JUMP_HEIGHT:
             player.is_jumping = False
             player.image = 'snowman_down' if player.is_moving_to_right else 'snowman_down_left'
-            print(f"player.is_moving_to_right {player.is_moving_to_right} {player.image}")
         else:
-            player.y -= 2
+            player.y -= height_y
 
+    if not player.colliderect(ground):
+        if not player.is_jumping:
+            player.y += height_y
     else:
-        flip_image()
+        player.flip_image()
 
-    for torch in torches:
-        if player.colliderect(torch):
-            set_player_hurt()
-
-
-def flip_image():
-    if player.colliderect(ground):
-        if player.is_moving_to_right:
-            current_player_image = PLAYER_IMAGE
-        else:
-            current_player_image = "snowman_left"
-        player.image = current_player_image
+    if points > 0:
+        for torch in torches:
+            torch.x -= 2
+            if player.colliderect(torch):
+                player.set_hurt()
+                sounds.eep.play()
+                points -= 1
 
 
 def on_mouse_down(pos):
     if player.collidepoint(pos):
-        set_player_hurt()
+        player.set_hurt()
 
 
 def on_key_up(key):
-    if key == keys.UP:
-        player.jump(ground)
-
-
-def set_player_hurt():
-    player.image = 'snowman_hurt'
-    # circle.angle += 180
-    # circle.y -= 20
-    clock.schedule_unique(set_player_normal, 0.2)
-
-
-def set_player_normal():
-    flip_image()
-    player.angle = 0
-    # circle._surf = pygame.transform.scale(circle._surf, (100, 100))
+    if key == keys.UP or keys.SPACE :
+        player.jump()
 
 
 pgzrun.go()
--- game/snowman.py
+++ game/snowman.py
@@ -1,15 +1,51 @@
 from pgzero.actor import Actor
+from pgzero.clock import clock
+from pgzero.loaders import sounds
+
+WIDTH = 1280  # ось X
 
 
 class Snowman(Actor):
-    transparent_step = 2
+    JUMP_HEIGHT = 300
+    PLAYER_IMAGE = 'snowman_right'
+    STEP = 3
 
     def __init__(self, image, **kwargs):
         super().__init__(image, **kwargs)
+        self.current_player_image = image
+
         self.is_moving_to_right = True
         self.is_jumping = False
 
-    def jump(self, ground):
-        if self.colliderect(ground):
+        self.ground = kwargs.get("ground")
+
+    def jump(self):
+        if self.colliderect(self.ground):
             self.image = "snowman_up" if self.is_moving_to_right else 'snowman_up_left'
             self.is_jumping = True
+
+    def set_hurt(self):
+        self.image = 'snowman_hurt'
+        clock.schedule_unique(self.set_normal, 0.2)
+
+    def set_normal(self):
+        self.flip_image()
+        self.angle = 0
+
+    def flip_image(self):
+        if self.colliderect(self.ground):
+            if self.is_moving_to_right:
+                self.current_player_image = self.PLAYER_IMAGE
+            else:
+                self.current_player_image = "snowman_left"
+            self.image = self.current_player_image
+
+    def update_x(self):
+        if self.is_moving_to_right:
+            self.x += self.STEP
+        else:
+            self.x -= self.STEP
+        player_half_width = self.width / 2
+        if self.x > WIDTH - player_half_width or self.x < player_half_width:
+            self.is_moving_to_right = not self.is_moving_to_right
+            self.flip_image()
