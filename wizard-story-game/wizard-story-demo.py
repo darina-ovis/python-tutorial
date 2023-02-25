deaf67c72563dd8f8686d6d26aca7cf19d912b1f
dnikolaeva <dnikolaeva@telematika.com>
2023-02-25 12:12:58 +0300

    avoid obstacles

--- wizard-story-game/wizard-story-demo.py
+++ wizard-story-game/wizard-story-demo.py
@@ -1,4 +1,5 @@
 import pgzrun
+from pygame.math import Vector2
 
 from Player import Player
 from Stone import Stone
@@ -17,13 +18,13 @@ LEVEL_MAP = [
     ['x', ' ', 'r', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 't', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', ' ', 'r', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 't', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', ' ', 'b', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
-    ['x', ' ', 'r', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
+    ['x', ' ', 'b', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', 'r', 'r', 'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', 'r', 't', 't', 't', 't', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['x', 'r', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
-    ['x', 'r', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
-    ['r', 'r', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
+    ['x', 'r', ' ', ' ', ' ', ' ', 't', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['r', 'r', ' ', ' ', ' ', ' ', 't', 't', 't', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
+    ['r', 'r', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x'],
     ['r', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
 ]
 
@@ -68,11 +69,31 @@ def draw():
 
 
 def update():
-    global player
+    global player, obstacles
+    pos: Vector2
     if player.direction.magnitude() != 0:
-        player.pos += player.direction.normalize() * player.speed
+        pos = player.direction.normalize() * player.speed
     else:
-        player.pos += player.direction * player.speed
+        pos = player.direction * player.speed
+
+    player.pos += pos
+    has_obstacle = False
+    current_obstacle = None
+    for obstacle in obstacles:
+        if player.colliderect(obstacle):
+            has_obstacle = True
+            current_obstacle = obstacle
+            break
+
+    if has_obstacle:
+        if player.direction.x == 1:
+            player.right = current_obstacle.left
+        elif player.direction.x == -1:
+            player.left = current_obstacle.right
+        if player.direction.y == 1:
+            player.bottom = current_obstacle.top
+        elif player.direction.y == -1:
+            player.top = current_obstacle.bottom
 
 
 def on_key_down(key):
