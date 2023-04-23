64a56d57928e760130db841da1acb478854a8c05
dnikolaeva <dnikolaeva@telematika.com>
2023-04-23 19:19:16 +0300

    player initial position is centered; added bridge upper image

--- wizard-story-game/Music.py
+++ wizard-story-game/Music.py
@@ -1,18 +1,23 @@
+import pgzero.tone
+
 from pgzero import tone
+from pygame import time
 
 
 class Music:
     def __init__(self, clock):
-        self.tone = None
+        self.tones = []
         self.melody = 'D4 C4 D4 E4 F4 E4 D4 C4 D4 C4 B3 C4 A3 A4 G4 A4 B4 C5 B4 A4 G4 A4 B4 C5 D5 E5'.split(' ')
+        for note in self.melody:
+            self.tones.append(tone.create(note, 1))
+            tone.note_queue.put(tone._convert_args(note, 1))
+        # self.channel = self.tones[0].play()
         self.clock = clock
 
-    def play(self):
-        delay = 1.0
-        for note in self.melody:
-            self.tone = tone.create(note, 1)
-            self.clock.schedule(self.play_one_note, delay)
-            delay += 1.0
-
-    def play_one_note(self):
-        self.tone.play()
+    def play(self, clock: time.Clock):
+        channel = self.tones[0].play()
+        channel.queue(self.tones[1])
+        channel.get_queue(self.tones[2])
+        # for sound in self.tones:
+        #     channel.queue(sound)
+        #     clock.tick(sound.get_length())
